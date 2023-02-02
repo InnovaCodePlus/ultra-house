c74dc0d7ece596f7576af65e1574eba3146d6efb
Mattias <duartemattias3@gmail.com>
2023-02-02 10:56:49 -0400

    end project

---
+++ assets/javascript/main.js
@@ -0,0 +1,24 @@
+// SCROLL REVEAL
+ScrollReveal().reveal('.scroll-up', { 
+    delay: 500, 
+    origin:'bottom',
+    distance: '60px',
+    duration: 2000
+});
+
+
+ScrollReveal().reveal('.scroll-left', { 
+    delay: 500, 
+    origin:'left',
+    distance: '60px',
+    duration: 2000
+});
+
+
+ScrollReveal().reveal('.scroll-right', { 
+    delay: 500, 
+    origin:'right',
+    distance: '60px',
+    duration: 2000
+});
+
